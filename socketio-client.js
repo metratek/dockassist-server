fc9fc77534fcb72d7a5b28242518eb71677ccb31
metratek <andreas.sakellariou@gmail.com>
2021-11-02 06:58:45 +0200

    fixed

--- socketio-client.js
+++ socketio-client.js
@@ -1,6 +1,6 @@
 const
     io = require("socket.io-client"),
-    ioClient = io.connect("http://5.189.167.236:3000");
+    ioClient = io.connect("https://5.189.167.236:3000");
 
 ioClient.on("seq-num", (msg) => console.info(msg));
 ioClient.on("timestamp", (msg) => console.info(msg));
